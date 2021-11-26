1b5a82856ef432b26ee8cce4a3d4a4594e70920b
Aliaksandr Zanouski <aliaksandrzanouski@wsa-050b-e1-pl.local>
2021-11-27 00:41:21 +0100

    refactoring

--- src/components/Avatar.js
+++
@@ -1,9 +0,0 @@
-import avatar from '../img/avatar.png'
-
-const Avatar = () => {
-    return (
-        <img className='avatar' src={avatar} alt='avatar-vlad' />
-    );
-};
-
-export default Avatar;
--- src/components/LinkBtn.js
+++
@@ -1,8 +0,0 @@
-const LinkBtn = ({networkLink}) => {
-    return (
-        <button className='btn-link'> {networkLink}
-        </button>
-    );
-};
-
-export default LinkBtn;
