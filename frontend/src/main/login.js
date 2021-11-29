e27e4c765192c34e3a19349b5694ae17d5a05646
Zanouskaya <volha_harahliad@icloud.com>
2021-11-29 21:35:22 +0100

    Link added

--- frontend/src/main/login.js
+++ frontend/src/main/login.js
@@ -1,10 +1,8 @@
-// import Link from 'react';
+import React from 'react';
+import {Link} from 'react-router-dom';
 import './main.css'
 import FormInput from "../components/FormInput";
 import {useState} from 'react';
-
-
-
 
 const LogIn = () => {
     const [values, setValues] = useState ({
@@ -40,7 +38,18 @@ const LogIn = () => {
         setValues( {...values, [e.target.name]: e.target.value });
     }
 
-    console.log(values);
+    // async function addUser(value) {
+    //   const response = await fetch ('http://127.001:5000/api/login', {
+    //     method: "POST",
+    //     body: JSON.stringify(value),
+    //     // headers: {
+    //     //   'Content-Type: aplication/json'
+    //     // }
+    //   });
+      // const data = await response.json();
+    //   console.log(data)
+    // }
+
 
     return (
         <div className='container-main'>
@@ -53,10 +62,13 @@ const LogIn = () => {
                                onChange={onChange}
                     />
                 )}
-
-                <button><a href='/profile'>LogIn</a></button>
+                <Link to='/profile'>
+                <button>LogIn</button>
+                </Link>
                 <h5>Don't have account?</h5>
-                <button className="btn-second"><a href="/step1">Create account</a></button>
+                <Link to="/step1">
+                <button className="btn-second">Create account</button>
+                </Link>
             </form>
         </div>
     );
